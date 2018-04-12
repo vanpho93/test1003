911ddd2167821d1d9335fcb2224cd631301716cc
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-12 17:49:18 +0700

    init test

---
+++ src/lib.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+    if (isNaN(a) || isNaN(b)) throw new Error('Type error');
+    return a + b;
+}
+
+function addCallback() {
+    setTimeout(() => {
+        if (isNaN(a) || isNaN(b)) return cb(new Error('Type error'));
+        cb(null, a + b);
+    }, 500);
+}
+
+function addPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (isNaN(a) || isNaN(b)) return reject(new Error('Type error'));
+            resolve(a + b);
+        }, 500);
+    });
+}
+
+module.exports = { add, addCallback, addPromise };
---
+++ test/lib.test.js
@@ -0,0 +1,7 @@
+const { add, addCallback, addPromise } = require('../src/lib');
+
+describe('Test add function', () => {
+    it('Can add 2 numbers', () => {
+        // throw new Error('123');
+    });
+});
