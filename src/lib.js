75cfdc8c6bf259ec7b3e8b9d2a2c6f8a561fcb97
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-12 18:07:57 +0700

    test add and addCallback

--- src/lib.js
+++ src/lib.js
@@ -3,14 +3,14 @@ function add(a, b) {
     return a + b;
 }
 
-function addCallback() {
+function addCallback(a, b, cb) {
     setTimeout(() => {
         if (isNaN(a) || isNaN(b)) return cb(new Error('Type error'));
         cb(null, a + b);
     }, 500);
 }
 
-function addPromise() {
+function addPromise(a, b) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (isNaN(a) || isNaN(b)) return reject(new Error('Type error'));
--- test/lib.test.js
+++ test/lib.test.js
@@ -2,6 +2,34 @@ const { add, addCallback, addPromise } = require('../src/lib');
 
 describe('Test add function', () => {
     it('Can add 2 numbers', () => {
-        // throw new Error('123');
+        const total = add(4, 5);
+        if (total !== 9) throw new Error('Loi');
+    });
+
+    it('Cannot add 2 strings', () => {
+        try {
+            const total = add('x', 'y');
+            throw new Error('Loi 1');
+        } catch (error) {
+            if (error.message !== 'Type error') throw new Error('Loi 2');
+        }
+    });
+});
+
+describe('Test addCallback function', () => {
+    it('Can add 2 numbers with callback', (done) => {
+        addCallback(4, 5, (error, result) => {
+            if (error !== null) throw new Error('Loi');
+            if (result !== 9) throw new Error('Loi');
+            done();
+        });
+    });
+
+    it('Cannot add 2 strings with callback', (done) => {
+        addCallback('x', 'y', (error, result) => {
+            if (result !== undefined) throw new Error('Loi');
+            if (error.message !== 'Type error') throw new Error('Loi');
+            done();
+        });
     });
 });
